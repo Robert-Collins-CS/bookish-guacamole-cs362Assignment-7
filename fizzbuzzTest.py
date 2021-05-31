637d6ec6d5c042d5cca6aa0f71d9a9f98fb43263
Robert-Collins-CS <72282644+Robert-Collins-CS@users.noreply.github.com>
2021-05-30 17:54:44 -0700

    implemented proper first test, Passes test
    passes test to see if the list contains "fizz" at the third index

--- fizzbuzzTest.py
+++ fizzbuzzTest.py
@@ -5,8 +5,8 @@ import fizzbuzz
 class TestStringMethods(unittest.TestCase):
 
     def test_fizzbuzz(self):
-        testList = fizzbuzz.populateList()
-        self.assertTrue(testList[3] == "Buzz")
+        testList = fizzbuzz.fizzBuzz((fizzbuzz.populateList()))
+        self.assertTrue(testList[2] == "Fizz")
 
 
 if __name__ == '__main__':
