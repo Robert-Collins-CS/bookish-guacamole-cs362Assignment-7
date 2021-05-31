1851f9f3820aeced58c908adde6e7ec5fc1be43c
Robert-Collins-CS <72282644+Robert-Collins-CS@users.noreply.github.com>
2021-05-30 18:33:14 -0700

    Completed 2nd Test, passes
    Passes check that year 4000 is a leap year and updates first test to determine that the year 3000 is in fact not a leap year

--- leapYearTests.py
+++ leapYearTests.py
@@ -4,6 +4,14 @@ import newLeapYear as nLYear
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_checkLeap(self):
+    def test_checkNotLeap(self):
         leapBool = nLYear.checkLeap(3000)
         self.assertFalse(leapBool)
+
+    def test_checkLeap(self):
+        leapBool = nLYear.checkLeap(4000)
+        self.assertTrue(leapBool)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
